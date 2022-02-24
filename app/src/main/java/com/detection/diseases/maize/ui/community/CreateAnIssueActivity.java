36dd91a3e56db55caaf6947e24ead2ff1c436571
ASIMWELA <augastinesimwela@gmail.com>
2022-02-24 20:24:04 +0200

    create issue screen design startup

---
+++ app/src/main/java/com/detection/diseases/maize/ui/community/CreateAnIssueActivity.java
@@ -0,0 +1,16 @@
+package com.detection.diseases.maize.ui.community;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.detection.diseases.maize.R;
+
+public class CreateAnIssueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_an_issue);
+    }
+}
