6bfad03fa1df1d99605c446be617555361426fdf
bed-com-08-19 <57199920+Haddad-NYIRENDA@users.noreply.github.com>
2022-02-25 17:17:54 +0200

    logged out user page

--- app/src/main/java/com/detection/diseases/maize/ui/camera/CameraActivity.java
+++ app/src/main/java/com/detection/diseases/maize/ui/camera/CameraActivity.java
@@ -112,14 +112,7 @@ public class CameraActivity extends AppCompatActivity implements CameraActivityC
                         Toast.LENGTH_SHORT).show();
                 finish();
             } else {
-                ProcessCameraProvider cameraProvider = null;
-                try {
-                    cameraProvider = cameraProviderListenableFuture.get();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                ProcessCameraProvider cameraProvider = cameraProviderListenableFuture.get();
                 startCamera(cameraProvider);
             }
         }
