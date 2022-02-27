9da845f17f19f9b9bb3f2dad3305c24a20b76638
ASIMWELA <augastinesimwela@gmail.com>
2022-02-27 04:53:12 +0200

    updated model results page adapter

--- app/src/main/java/com/detection/diseases/maize/ui/modelresults/ModelResultsActivity.java
+++ app/src/main/java/com/detection/diseases/maize/ui/modelresults/ModelResultsActivity.java
@@ -1,29 +1,41 @@
 package com.detection.diseases.maize.ui.modelresults;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.detection.diseases.maize.R;
 import com.detection.diseases.maize.helpers.AppConstants;
 import com.detection.diseases.maize.helpers.EDiseases;
+import com.detection.diseases.maize.ui.modelresults.model.ImageResources;
 import com.detection.diseases.maize.ui.modelresults.model.ModelResults;
 import com.google.android.material.chip.Chip;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ModelResultsActivity extends AppCompatActivity {
     private ListView lvRecommendations, lvSymptoms;
     private Chip cpBackArrow;
     private TextView tvDiseaseName, tvRecommendations, tvSymptoms;
-    private ViewPager viewPager;
     private ImageView ivMoreMenu;
+    private ViewPager2 viewPager2;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +45,7 @@ public class ModelResultsActivity extends AppCompatActivity {
         Gson gson = new Gson();
         ModelResults results = gson.fromJson(getIntent().getStringExtra(AppConstants.MODEL_RESULTS), ModelResults.class);
         tvDiseaseName.setText(results.getDiseaseName());
-        if(results.getDiseaseName().equals("Health")){
+        if (results.getDiseaseName().equals("Health")) {
             tvRecommendations.setText("Keeping The Crop Healthy");
             tvSymptoms.setText("Signs for a healthy crop");
         }
@@ -42,41 +54,71 @@ public class ModelResultsActivity extends AppCompatActivity {
         ArrayAdapter<String> symptomsAdapter = new ArrayAdapter<>(this,
                 R.layout.disease_symptoms_row, results.getSymptoms());
 
-        int[] common_rust = {R.drawable.common_rust_1, R.drawable.common_rsut_2, R.drawable.common_rust_3};
-        int[] blight_spot = {R.drawable.blight_1, R.drawable.blight_2, R.drawable.blight_3};
-        int[] grayLeaf = {R.drawable.gray_leaf_1, R.drawable.gray_leaf_2, R.drawable.gray_leaf_3};
-        int[] health = {R.drawable.corn_health_1, R.drawable.corn_health_2, R.drawable.corn_health_3};
+        List<ImageResources> commonRust = Stream.of(
+                new ImageResources(R.drawable.common_rust_1),
+                new ImageResources(R.drawable.common_rsut_2),
+                new ImageResources(R.drawable.common_rust_3)).collect(Collectors.toList());
 
 
-        //set adapters
+        List<ImageResources> nothernLeafBlight = Stream.of(
+                new ImageResources(R.drawable.blight_1),
+                new ImageResources(R.drawable.blight_2),
+                new ImageResources(R.drawable.blight_3)).collect(Collectors.toList());
+
+
+        List<ImageResources> health = Stream.of(
+                new ImageResources(R.drawable.corn_health_1),
+                new ImageResources(R.drawable.corn_health_2),
+                new ImageResources(R.drawable.corn_health_3)).collect(Collectors.toList());
+
+
+        List<ImageResources> gray = Stream.of(
+                new ImageResources(R.drawable.gray_leaf_1),
+                new ImageResources(R.drawable.gray_leaf_2),
+                new ImageResources(R.drawable.gray_leaf_3)).collect(Collectors.toList());
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer((page, position) -> {
+            page.setTranslationX(-100 * position);
+        });
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.setOffscreenPageLimit(3);
+
+
+        SliderAdapter sliderAdapter = null;
+
         lvSymptoms.setAdapter(symptomsAdapter);
         lvRecommendations.setAdapter(recommendationAdapter);
 
-        ViewPagerAdapter viewPagerAdapter = null;
-        if(results.getDiseaseName().equals(EDiseases.COMMON_RUST.label)){
-            viewPagerAdapter = new ViewPagerAdapter(this, common_rust);
+
+        if (results.getDiseaseName().equals(EDiseases.COMMON_RUST.label)) {
+            sliderAdapter = new SliderAdapter(commonRust, viewPager2);
         }
-        if(results.getDiseaseName().equals(EDiseases.GRAY_LEAF_SPOT.label)){
-            viewPagerAdapter = new ViewPagerAdapter(this, grayLeaf);
+        if (results.getDiseaseName().equals(EDiseases.GRAY_LEAF_SPOT.label)) {
+            sliderAdapter = new SliderAdapter(gray, viewPager2);
         }
-        if(results.getDiseaseName().equals(EDiseases.NORTHERN_LEAF_BLIGHT.label)){
-            viewPagerAdapter = new ViewPagerAdapter(this, blight_spot);
+        if (results.getDiseaseName().equals(EDiseases.NORTHERN_LEAF_BLIGHT.label)) {
+            sliderAdapter = new SliderAdapter(nothernLeafBlight, viewPager2);
         }
-        if(results.getDiseaseName().equals(EDiseases.HEALTH.label)){
-            viewPagerAdapter = new ViewPagerAdapter(this, health);
+        if (results.getDiseaseName().equals(EDiseases.HEALTH.label)) {
+            sliderAdapter = new SliderAdapter(health, viewPager2);
         }
-        viewPager.setAdapter(viewPagerAdapter);
-        cpBackArrow.setOnClickListener(v->onBackPressed());
+        viewPager2.setAdapter(sliderAdapter);
+        cpBackArrow.setOnClickListener(v -> onBackPressed());
     }
 
-    private void initViews(){
+    private void initViews() {
         lvRecommendations = findViewById(R.id.model_results_lv_recommendations);
         lvSymptoms = findViewById(R.id.model_results_lv_symptoms);
-        cpBackArrow  = findViewById(R.id.model_results_cp_back);
+        cpBackArrow = findViewById(R.id.model_results_cp_back);
         tvDiseaseName = findViewById(R.id.model_results_activity_tv_disease_name);
         ivMoreMenu = findViewById(R.id.model_results_iv_more_menu);
         tvRecommendations = findViewById(R.id.model_results_tv_recommendations);
         tvSymptoms = findViewById(R.id.model_results_tv_symptoms);
-        viewPager = findViewById(R.id.view_pager_model_results);
+        viewPager2 = findViewById(R.id.images_preview_pager);
     }
+
 }
---
+++ app/src/main/java/com/detection/diseases/maize/ui/modelresults/SliderAdapter.java
@@ -0,0 +1,70 @@
+package com.detection.diseases.maize.ui.modelresults;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.detection.diseases.maize.R;
+import com.detection.diseases.maize.ui.modelresults.model.ImageResources;
+import com.makeramen.roundedimageview.RoundedImageView;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder> {
+    private List<ImageResources> sliderItems;
+    private ViewPager2 viewPager2;
+
+    SliderAdapter(List<ImageResources> sliderItems, ViewPager2 viewPager2) {
+        this.sliderItems = sliderItems;
+        this.viewPager2 = viewPager2;
+    }
+
+    @NonNull
+    @Override
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new SliderViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.model_result_image_holder, parent, false
+                ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+        holder.setImage(sliderItems.get(position));
+        if (position == sliderItems.size() - 2) {
+            viewPager2.post(runnable);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return sliderItems.size();
+    }
+
+    static class SliderViewHolder extends RecyclerView.ViewHolder {
+        private RoundedImageView imageView;
+
+
+        SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_image_view_holder);
+        }
+
+        void setImage(ImageResources sliderItems) {
+            Picasso.get().load(sliderItems.getImage()).fit().into(imageView);
+        }
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
+}
