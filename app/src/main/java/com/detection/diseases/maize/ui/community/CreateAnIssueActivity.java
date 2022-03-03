514cb64c7374c8355b0d8678c831b2ef4fd27618
ASIMWELA <augastinesimwela@gmail.com>
2022-03-03 22:03:48 +0200

    add an image

--- app/src/main/java/com/detection/diseases/maize/ui/community/CreateAnIssueActivity.java
+++ app/src/main/java/com/detection/diseases/maize/ui/community/CreateAnIssueActivity.java
@@ -1,42 +1,35 @@
 package com.detection.diseases.maize.ui.community;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.widget.NestedScrollView;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.detection.diseases.maize.R;
 import com.detection.diseases.maize.helpers.RealPathUtil;
-import com.detection.diseases.maize.ui.camera.CameraActivity;
 import com.detection.diseases.maize.ui.community.model.GalleryImageModel;
-import com.detection.diseases.maize.ui.modelresults.model.ImageIdsHolder;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import lombok.SneakyThrows;
 
@@ -46,10 +39,15 @@ public class CreateAnIssueActivity extends AppCompatActivity {
     private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 200;
     private ConstraintLayout bottomSheetView;
     private BottomSheetBehavior<?> bottomSheetBehavior;
-    private ImageView ivUpDown;
+    private ImageView ivUpDown, iVSelectedImage, ivCancelImageSelection;
     private GridView imageGridContainer;
     private List<File> images = new ArrayList<>();
     private CoordinatorLayout bottomSheetHolder;
+    private Button btnCreateIssue;
+    private EditText edIssueQuestion, edIssueDescription;
+    private File fSelectedImage;
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
@@ -64,7 +62,6 @@ public class CreateAnIssueActivity extends AppCompatActivity {
             loadImagesAndAttachToAdapter();
         }
 
-
         ivUpDown.setOnClickListener(v -> {
             if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
                 bottomSheetBehavior.setDraggable(true);
@@ -77,9 +74,32 @@ public class CreateAnIssueActivity extends AppCompatActivity {
 
         imageGridContainer.setOnItemClickListener((adapterView, view, position, id) -> {
             GalleryImageModel selectedImage = (GalleryImageModel)adapterView.getItemAtPosition(position);
-            Toast.makeText(CreateAnIssueActivity.this, selectedImage.getImage().getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            fSelectedImage = selectedImage.getImage();
+            Picasso.get().load(fSelectedImage).fit().centerCrop().into(iVSelectedImage);
+            iVSelectedImage.setVisibility(View.VISIBLE);
+            bottomSheetBehavior.setHideable(true);
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+            ivCancelImageSelection.setVisibility(View.VISIBLE);
 
         });
+
+        ivCancelImageSelection.setOnClickListener(v->{
+            ivCancelImageSelection.setVisibility(View.GONE);
+            iVSelectedImage.setVisibility(View.GONE);
+            iVSelectedImage.setImageDrawable(null);
+            bottomSheetBehavior.setHideable(false);
+            bottomSheetBehavior.setDraggable(true);
+            fSelectedImage = null;
+        });
+
+
+        edIssueDescription.setFocusable(false);
+        edIssueQuestion.setFocusable(false);
+
+
+        edIssueQuestion.setOnClickListener(view -> edIssueQuestion.setFocusable(true));
+
+        edIssueDescription.setOnClickListener(view -> edIssueDescription.setFocusable(true));
 
         bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
@@ -88,11 +108,11 @@ public class CreateAnIssueActivity extends AppCompatActivity {
                    bottomSheetBehavior.setDraggable(false);
                 }
             }
-
             //change the state of the arrow icon
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
                 ivUpDown.setRotation(slideOffset * 180);
+
             }
         });
 
@@ -118,6 +138,11 @@ public class CreateAnIssueActivity extends AppCompatActivity {
         ivUpDown = findViewById(R.id.bttom_sheet_up_arrow);
         imageGridContainer = findViewById(R.id.bottom_sheet_images_grid);
         bottomSheetHolder = findViewById(R.id.coordiantor_layout_sheet_holder);
+        btnCreateIssue = findViewById(R.id.create_issue_btn_send_issue);
+        edIssueQuestion = findViewById(R.id.edt_create_issue_question);
+        edIssueDescription=findViewById(R.id.ed_create_issue_question_desc);
+        iVSelectedImage = findViewById(R.id.iv_create_an_issue_selected_image);
+        ivCancelImageSelection = findViewById(R.id.iv_cancel_image_selectiton);
     }
 
     public boolean checkAndRequestPermissions() {
