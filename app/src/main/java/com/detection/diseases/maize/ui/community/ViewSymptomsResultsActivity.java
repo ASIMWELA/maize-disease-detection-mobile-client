15f584df6299d0f2400978cee9c520b57eb5d610
KndwaniNang2 <Knangona@gmail.com>
2022-03-14 17:38:50 +0200

    ViewSymptomsResultsActivity

---
+++ app/src/main/java/com/detection/diseases/maize/ui/community/ViewSymptomsResultsActivity.java
@@ -0,0 +1,14 @@
+package com.detection.diseases.maize.ui.community;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.detection.diseases.maize.R;
+import android.os.Bundle;
+
+public class ViewSymptomsResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_symptoms_results);
+    }
+}
